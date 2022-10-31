26c9886738cd63fa859ed78fa029598cb24a8b8a
Aliripoll <aripollortega@gmail.com>
2022-10-31 10:14:53 +0100

    add event click

--- main.js
+++ main.js
@@ -1,26 +1,42 @@
 'use strict';
 
 const btnSelect = document.querySelector('.btnSelect');
+const battleBtn = document.querySelector('.battleBtn');
 
 function getRandomEvilRace(max) {
     let race = '';   
     let randomNumber = Math.ceil(Math.random() * max);
     if (randomNumber === 1) {
-        race = 'Sureños malos con fuerza (2)';
+        race = 2;
     }
     else if (randomNumber === 2) {
-        race = 'Orcos con fuerza (2)';
+        race = 2;
     }
     else if (randomNumber === 3) {
-        race = 'Goblins con fuerza (2)';
+        race = 2;
     }
     else if (randomNumber === 4) {
-        race = 'Huargos con fuerza (3)';
+        race = 3;
     }
     else {
-        race = 'Trolls con fuerza(5)';
+        race = 5;
     }   
     return race;
 } 
 
 const finalEvilRace = getRandomEvilRace(5);
+
+battleBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    let result = '';
+    if (btnSelect.value > finalEvilRace) {
+        result = 'Ha ganado el Ejército del Bien! Enhorabuena.';
+    } 
+    else if (finalEvilRace > btnSelect.value) {
+        result = 'Ha ganado el Ejército del Mal! Vuelve a Intentarlo.';
+    } 
+    else {
+        result = 'Empate';
+    } 
+    console.log(result);
+});
